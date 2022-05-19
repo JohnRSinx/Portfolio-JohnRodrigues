cadc5a3268a21d4a0787f45134f9ce7288a8e6a2
John Rodrigues <johnrodrigues97106@gmail.com>
2022-05-19 13:29:11 -0300

    correção texto

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,7 +8,7 @@ export function About() {
         <h1>John Rodrigues </h1>
         <h2>Front-End Developer</h2>
         <p>
-          Olá, me chamo John , tenho 24 anos , entrei no mundo da  programação no final de 2021,quando um amigo me apresentou um curso, desde  então venho estudando e me aperfeiçoando.Meu foco hoje é o conhecimento pois me apaixonei pela área e não existe outra profissão que eu queira seguir.
+          Olá me chamo John , tenho 24 anos e entrei no mundo da  programação no final de 2021,quando um amigo me apresentou um curso, desde  então venho estudando e me aperfeiçoando.Meu foco hoje é o conhecimento pois me apaixonei pela área e não existe outra profissão que eu queira seguir.
         </p>
       </div>
       <img src={myProfile_img} alt="foto-minha" />
