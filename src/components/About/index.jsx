1e84f37828b367a988eaa82b63de0cc0fcd31a10
John Rodrigues <johnrodrigues97106@gmail.com>
2023-04-28 19:25:52 +0000

    atualização na foto e sobre mim

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,7 +8,15 @@ export function About() {
         <h1>John Rodrigues </h1>
         <h2>Front-End Developer</h2>
         <p>
-          Olá me chamo John , tenho 24 anos e entrei no mundo da  programação no final de 2021,quando um amigo me apresentou um curso, desde  então venho estudando e me aperfeiçoando.Meu foco hoje é o conhecimento pois me apaixonei pela área e não existe outra profissão que eu queira seguir.
+        Olá, meu nome é John Rodrigues e sou Front end Developer. Com 1 anos e 6 meses  de experiência em html , css , javascript , typescript e reactjs, tenho paixão por desafios, resolução de problemas, fazer a diferença, etc.
+
+Meu objetivo é Desenvolvedor Front End , e para isso, tenho desenvolvido habilidades em Reactjs e Typescript. Sou capaz de colaborar e trabalhar em conjunto com colegas de trabalho para atingir objetivos comuns , o que me permite entregar resultados de qualidade e atender às expectativas dos meus clientes e colegas de trabalho.
+
+Além disso, tenho forte interesse em Nextjs e  Inteligencia artificial. Estou sempre buscando me atualizar e aprender novas habilidades para me manter em constante evolução profissional.
+
+Se você está em busca de um profissional comprometido, proativo e com habilidades em Desenvolvedor Fron End, entre em contato comigo para discutirmos possíveis oportunidades de colaboração.
+
+Obrigado pela visita ao meu perfil!
         </p>
       </div>
       <img src={myProfile_img} alt="foto-minha" />
